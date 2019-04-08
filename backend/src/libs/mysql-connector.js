953183efe67ed00f13ff0ca23db615b6f18fe3d4
Matheus Cruz <Matheus@MacMini-Matheus-Cruz.local>
2019-04-08 17:41:25 -0300

    docker and db

---
+++ backend/src/libs/mysql-connector.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql'),
+      pool = mysql.createPool({
+        connectionLimit : 10,
+        host: process.env.DBhost,
+        user: process.env.DBuser,
+        password: process.env.DBpassword,
+        database: process.env.DBname,
+      });
+
+const DB = (function () {
+
+  function _query(query, params, callback) {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        callback(err);
+        return 
+      }
+
+      connection.query(query, params, function (err, rows) {
+        connection.release();
+        callback(err, rows);
+      });
+
+      connection.on('error', function (err) {
+        connection && connection.release();
+        callback(err);
+      });
+      
+    });
+
+  }
+  return {
+    query: _query
+  }
+
+})();
+
+module.exports = DB;
--- backend/src/schemas/auth-login-body.js
+++ backend/src/schemas/auth-login-body.js
@@ -1,8 +1,10 @@
 const joi = require('joi');
 
+const PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W+)(?=^.{6,50}$).*$/;
+
 const schema = joi.object().keys({
   username: joi.string().alphanum().min(4).max(12).required(),
-  password: joi.string().regex(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W+)(?=^.{6,50}$).*$/).required(),
+  password: joi.string().regex(PASSWORD_REGEX).required(),
 }).required();
 
 module.exports = schema;
